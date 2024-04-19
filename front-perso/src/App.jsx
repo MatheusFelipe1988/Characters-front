bee72172126404c9bdcfe1afb06e27c1a4a51221
MFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-04-19 14:58:40 -0400

    Upload
    upando arquivos para a aplicação

---
+++ front-perso/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h2>Max Verstappen</h2>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0tmi66yM9XIyQu-sEpwErBUa4-zWT3ZzfkhkGhNtTaw&s"></img>
+      </div>
+    </>
+  );
+}
+
+export default App;
